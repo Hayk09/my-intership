dd9cfaee7445815828df861ca30ec5a1a6bd32af
hayk09 <hayksargsyan0909@gmail.com>
2022-02-27 14:35:41 +0400

    create router provider

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { RouterProvider } from "./provider";
+import "./Styles/app.scss"
+
+const App = () => {
+  return (
+    <div >
+       <RouterProvider/>
+    </div>
+  );
+}
+
+export default App;
