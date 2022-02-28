f6ae8926e3a65a09cb1c0c846846b54ee95485de
arturasatrya <asatryanartur.2008@gmail.com>
2022-02-28 22:02:44 +0400

    change/Navbar/index.js

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,19 +1,48 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from "react";
+import "./index.scss";
+import Menu from "../../assets/Group 2.png";
+import Kahuyq from "../../assets/P7uSyhzrGW23dOIqvPzCwDFqGrP8cRGbA6wFyErM1vX5bwBiMQkYyT7rcprgOkty1Vj+UxHv62_FBwKF7qjIBjj8llu6wC8WlT7AjmgfzuWurYYVq0Zoew7VaKlK4NkZF3ghZmC9avYMdF0CsbCjRZp55CRQ7ACBA0cUNA4CBYxm_ldDkQRtDEO_IgFuOWSM8uSfYDasuhhMJ1n8TyDQskM.png";
 
 const Navbar = () => {
   return (
-    <div>
-      <Link to='/'>
-      <p>HomePage</p>
-      </Link>
-
-      <Link to='/about'>
-         <p>About</p> 
-      </Link>
-        
+    <div className="all">
+      <img src={Kahuyq} alt="kk" className="glav" />
+      <div className="menu">
+        <img src={Menu} alt="menu" />
+      </div>
+      <div className="header">
+        <a href="/" className="q">
+          <p className="header1">Woodoo</p>
+        </a>
+      </div>
+      <div className="signin">
+        <button className="signin1">Sign in</button>
+      </div>
+      <div className="signup">
+        <button className="signup1">Sign up</button>
+      </div>
+      <div className="box">
+        <p className="text">WooDoo</p>
+      </div>
+      <div className="box1">
+        <p className="text1"> Design Studio</p>
+      </div>
+      <div className="box2">
+        <pre>
+          <p className="text2">
+            Here you can order any type of
+            <br />
+            furniture making your space beautiful,
+            <br />
+            simple but significant.
+          </p>
+        </pre>
+      </div>
+      <div className="box3">
+        <button className="button">EXPLORE</button>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
