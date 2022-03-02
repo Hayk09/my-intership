0242e68bca45072f7df19f0f8d84c9702db35798
Arturkir12 <kirakosyanartur2009@gmail.com>
2022-03-02 21:30:45 +0000

    create js.kitchen

--- src/pages/Kitchen/index.js
+++ src/pages/Kitchen/index.js
@@ -1,8 +1,13 @@
 import React from 'react'
+import "./index.scss"
+import kitchen from "../../components/Assets/Rectangle 22 (1).png"
 
 const Kitchen = () => {
   return (
-    <div>kitchen</div>
+    <div
+    className='div-1'>
+         <img src={kitchen} alt="kitchen" className='kitchen'/>
+    </div>
   )
 }
 
