d4bc5d65ae4357c14a4d24435e8f2c9162831b73
arturasatrya <asatryanartur.2008@gmail.com>
2022-03-03 18:16:33 +0400

    change/layouts/AuthLayout.js

--- src/layouts/AuthLayout.js
+++ src/layouts/AuthLayout.js
@@ -1,18 +1,17 @@
 import React from "react";
 import Card from "../components/Card";
-import Navbar from '../components/Navbar'
-
+import Footer from "../components/Footer";
+import Navbar from "../components/Navbar";
 
 const AuthLayout = ({ children }) => {
-    return (
-        <div>
-            <Navbar />
-            <Card />
-            <div>{children}</div>
-         
-        </div>
+  return (
+    <div>
+      <Navbar />
+      <Card />
+      <Footer />
+      <div>{children}</div>
+    </div>
+  );
+};
 
-    )
-}
-
-export default AuthLayout
+export default AuthLayout;
