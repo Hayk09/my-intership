ed52104628bfb3f26c049d6d152f5192456d49eb
Arturkir12 <kirakosyanartur2009@gmail.com>
2022-03-06 22:16:33 +0000

    change HomePage/AuthLayouth

--- src/layouts/AuthLayout.js
+++ src/layouts/AuthLayout.js
@@ -2,12 +2,14 @@ import React from "react";
 import Navbar from '../components/Navbar'
 import Footer from "../components/Footer"
 import Card from "../components/Card";
+import HomePage from "../components/HomePage"
 
 
 const AuthLayout = ({ children }) => {
     return (
         <div>
             <Navbar />
+            <HomePage/>
             <Card/>
             <Footer/>
             <div>{children}</div>
