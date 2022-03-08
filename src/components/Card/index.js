a0f29f7a4661d8a7ecc883914b2b7ccaef4eafb5
Arturkir12 <kirakosyanartur2009@gmail.com>
2022-03-08 09:36:05 +0000

    cange and create swiper

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,20 +1,37 @@
-import React from "react";
 import "./index.scss"
 import photo1 from "../Assets/Rectangle 23.png"
 import photo2 from "../Assets/Rectangle 24.png"
 import photo3 from "../Assets/Rectangle 25.png"
 import photo4 from "../Assets/Rectangle 26.png"
+import React, { useRef, useState } from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
 
+
+// import required modules
+import { Pagination, Navigation } from "swiper";
 
 const Card = () => {
     return(
        <div className="divcrd">
            <p className="recomended"><b>Recomended for you</b></p>
-            <img className="photo1" src={photo1}/>
-            <img className="photo2" src={photo2}/>
-            <img className="photo3" src={photo3}/>
-            <img className="photo4" src={photo4}/>
-       </div>
+      <Swiper
+        slidesPerView={4}
+        spaceBetween={30}
+        slidesPerGroup={1}
+        loop={true}
+        loopFillGroupWithBlank={true}
+        navigation={true}
+        modules={[Pagination, Navigation]}
+      >
+        <SwiperSlide><img className="photo1" src={photo1}/></SwiperSlide>
+        <SwiperSlide><img className="photo2" src={photo2}/></SwiperSlide>
+        <SwiperSlide><img className="photo3" src={photo3}/></SwiperSlide>
+        <SwiperSlide><img className="photo4" src={photo4}/></SwiperSlide>
+      </Swiper>
+            </div>
     )
 }
 
