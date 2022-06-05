3df648704626075a93434b9cb39a2625dc8242aa
Arturkir12 <kirakosyanartur2009@gmail.com>
2022-06-05 18:48:18 +0100

    create and change navbar

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,25 +1,69 @@
 import React from 'react'
-import { Box } from '@mui/material'
-import {COLOR,BORDRADIUS,FONTS} from '../../constant/index'
-// import MenuButtonIcon from "../Assets/Group 2.png"
+import { 
+  Box,
+  Button,
+} from '@mui/material'
+import {COLOR,FONTS,FONTFAMILY,MARGINS,BORDRADIUS} from '../../constant/index'
+import { CssBaseline } from '@mui/material'
+import { useHistory } from 'react-router-dom'
+
 
 const Navbar = () => {
- 
+  const history = useHistory()
   return (
-    <Box
-      sx={{
-        display: 'flex',
-        backgroundColor:COLOR.brown ,
-        height: '90px',
-        width: '100%',
-        justifyContent: 'space-around',
-        alignItems: 'center',
-        borderRadius: BORDRADIUS.lg
-      }}
-    >
-       <Box>
-         
-       </Box>
+    <Box>
+      <CssBaseline/>
+      <Box sx={{
+        display:"flex",
+        justifyContent:"center",
+        boxShadow:"3",
+        height:"90px",
+        alignItems:"center"
+      }}>
+        <Box sx={{
+           fontFamily:FONTFAMILY.montserrat,
+           fontSize:FONTS.body3
+        }}>Great</Box>
+        <Box sx={{
+           fontFamily:FONTFAMILY.montserrat,
+           fontSize:FONTS.body3,
+           color:COLOR.yellow
+        }}>STAR</Box>
+        <Box sx={{
+          fontFamily:FONTFAMILY.lobster,
+          fontSize:FONTS.smoll,
+          mt:MARGINS.mt
+        }}>
+          GS
+        </Box>
+        <Button sx={{
+          color:COLOR.black,
+          width:"130px"
+        }}>
+          Launch
+        </Button>
+        <Button sx={{
+          color:COLOR.black,
+          width:"130px"
+        }}>
+          Manage
+        </Button>
+        <Button sx={{
+          color:COLOR.black,
+          width:"130px"
+        }}>
+          Pricing
+        </Button>
+        <Button sx={{
+            color:COLOR.amber,
+            bgcolor:COLOR.yellow3,
+            borderRadius:BORDRADIUS.xxl
+        }} variant="text" onClick={() => (
+          history.push("/login")
+        )}>
+          LOG IN
+        </Button>
+      </Box>
     </Box>
   )
 }
