1cdc78a10d47252dc38b4710eb1f9d5ce0942428
arturasatrya <asatryanartur.2008@gmail.com>
2022-07-03 13:43:25 +0400

    change/footer/create/about

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,4 +1,13 @@
-import { Box, Link, ImageListItem, TextField } from "@mui/material";
+import {
+  Box,
+  Link,
+  ImageListItem,
+  TextField,
+  Button,
+  Tooltip,
+  Stack,
+  ImageList,
+} from "@mui/material";
 import React from "react";
 import {
   COLOR,
@@ -66,12 +75,31 @@ const Footer = () => {
       </Box>
       <Box
         sx={{
-          ml: "680px",
+          ml: "500px",
           mt: "-50px",
         }}
       >
+        <Link href="/about" sx={{ color: COLOR.yellow }}>
+          <Tooltip title="You can find my location">
+            <Button
+              sx={{
+                color: COLOR.yellow,
+              }}
+            >
+              About
+            </Button>
+          </Tooltip>
+        </Link>
+      </Box>
+      <Box
+        sx={{
+          display:"flex",
+          ml: "680px",
+          mt: "-40px",
+        }}
+      >
         <TextField
-          sx={{ bgcolor: COLOR.yellow, width: "222px" }}
+          sx={{ bgcolor: COLOR.yellow, width: "222px",height:"40px" }}
           placeholder="Search"
         />
         <Box
@@ -96,35 +124,45 @@ const Footer = () => {
           You can find me here :)
         </Box>
       </Box>
-      <ImageListItem
-        sx={{
-          borderRadius: BORDRADIUS.xxl,
-          width: "60px",
-          ml: "1450px",
-          mt: "-100px",
-        }}
-      >
-        <Link href="https://www.instagram.com/">
-          <ImageListItem>
-            <img src="	https://www.danoneinstitute.org/wp-content/uploads/2020/06/insta-logo.png" />
-          </ImageListItem>
-        </Link>
-        <Link href="https://www.facebook.com/campaign/landing.php?campaign_id=1551277091&extra_1=s%7Cc%7C355201143755%7Ce%7Cfacebook%27%7C&placement=&creative=355201143755&keyword=facebook%27&partner_id=googlesem&extra_2=campaignid%3D1551277091%26adgroupid%3D58439965909%26matchtype%3De%26network%3Dg%26source%3Dnotmobile%26search_or_content%3Ds%26device%3Dc%26devicemodel%3D%26adposition%3D%26target%3D%26targetid%3Dkwd-362360550869%26loc_physical_ms%3D9070053%26loc_interest_ms%3D%26feeditemid%3D%26param1%3D%26param2%3D&gclid=Cj0KCQjw2MWVBhCQARIsAIjbwoOxZrod8Lpfjdk4xo0R5v9U-qeumzJHEk2GpLlGekrg8mURLF6AsNAaApX-EALw_wcB">
-          <ImageListItem
-            sx={{
-              width: "55px",
-              ml: "2px",
-            }}
-          >
-            <img src="	https://cdn.worldvectorlogo.com/logos/facebook-3-3.svg" />
-          </ImageListItem>
-        </Link>
-        <Link href="https://www.whatsapp.com/?lang=ru://www.facebook.com/campaign/landing.php?campaign_id=1551277091&extra_1=s%7Cc%7C355201143755%7Ce%7Cfacebook%27%7C&placement=&creative=355201143755&keyword=facebook%27&partner_id=googlesem&extra_2=campaignid%3D1551277091%26adgroupid%3D58439965909%26matchtype%3De%26network%3Dg%26source%3Dnotmobile%26search_or_content%3Ds%26device%3Dc%26devicemodel%3D%26adposition%3D%26target%3D%26targetid%3Dkwd-362360550869%26loc_physical_ms%3D9070053%26loc_interest_ms%3D%26feeditemid%3D%26param1%3D%26param2%3D&gclid=Cj0KCQjw2MWVBhCQARIsAIjbwoOxZrod8Lpfjdk4xo0R5v9U-qeumzJHEk2GpLlGekrg8mURLF6AsNAaApX-EALw_wcB">
-          <ImageListItem>
-            <img src="https://conniescomfortsuites.com/wp-content/uploads/2018/03/WHATSAPP-LOGO.png" />
-          </ImageListItem>
-        </Link>
-      </ImageListItem>
+      <Stack direction="row">
+        <Box
+          sx={{
+            display:"flex",
+            gap:"5px",
+            width: "260px",
+            ml: "1250px",
+            mt: "30px",
+          }}
+        >
+          <Link href="https://www.instagram.com/">
+            <ImageListItem
+              sx={{
+                width: "30px",
+              }}
+            >
+              <img src="	https://www.danoneinstitute.org/wp-content/uploads/2020/06/insta-logo.png" />
+            </ImageListItem>
+          </Link>
+          <Link href="https://www.facebook.com/campaign/landing.php?campaign_id=1551277091&extra_1=s%7Cc%7C355201143755%7Ce%7Cfacebook%27%7C&placement=&creative=355201143755&keyword=facebook%27&partner_id=googlesem&extra_2=campaignid%3D1551277091%26adgroupid%3D58439965909%26matchtype%3De%26network%3Dg%26source%3Dnotmobile%26search_or_content%3Ds%26device%3Dc%26devicemodel%3D%26adposition%3D%26target%3D%26targetid%3Dkwd-362360550869%26loc_physical_ms%3D9070053%26loc_interest_ms%3D%26feeditemid%3D%26param1%3D%26param2%3D&gclid=Cj0KCQjw2MWVBhCQARIsAIjbwoOxZrod8Lpfjdk4xo0R5v9U-qeumzJHEk2GpLlGekrg8mURLF6AsNAaApX-EALw_wcB">
+            <ImageListItem
+              sx={{
+                width: "26px",
+              }}
+            >
+              <img src="	https://cdn.worldvectorlogo.com/logos/facebook-3-3.svg" />
+            </ImageListItem>
+          </Link>
+          <Link href="https://www.whatsapp.com/?lang=ru://www.facebook.com/campaign/landing.php?campaign_id=1551277091&extra_1=s%7Cc%7C355201143755%7Ce%7Cfacebook%27%7C&placement=&creative=355201143755&keyword=facebook%27&partner_id=googlesem&extra_2=campaignid%3D1551277091%26adgroupid%3D58439965909%26matchtype%3De%26network%3Dg%26source%3Dnotmobile%26search_or_content%3Ds%26device%3Dc%26devicemodel%3D%26adposition%3D%26target%3D%26targetid%3Dkwd-362360550869%26loc_physical_ms%3D9070053%26loc_interest_ms%3D%26feeditemid%3D%26param1%3D%26param2%3D&gclid=Cj0KCQjw2MWVBhCQARIsAIjbwoOxZrod8Lpfjdk4xo0R5v9U-qeumzJHEk2GpLlGekrg8mURLF6AsNAaApX-EALw_wcB">
+            <ImageListItem
+              sx={{
+                width: "30px",
+              }}
+            >
+              <img src="https://conniescomfortsuites.com/wp-content/uploads/2018/03/WHATSAPP-LOGO.png" />
+            </ImageListItem>
+          </Link>
+        </Box>
+      </Stack>
     </Box>
   );
 };
